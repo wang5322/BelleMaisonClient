7ba33d1e7f89fe5cf33114fce16d0de8f4810c07
ShiyuCCC <115126635+shiyuCCC@users.noreply.github.com>
2023-10-22 23:27:58 -0400

    Update AdminProperty.js
    added modal

--- src/pages/AdminProperty.js
+++ src/pages/AdminProperty.js
@@ -1,95 +1,114 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import "./Admin.css"
-import { Link } from 'react-router-dom';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import "./Admin.css";
+import { Link } from "react-router-dom";
+import ModalMessage from "../components/ModalMessage";
 
 function AdminProperty() {
-    const [propertyList, setPropertyList] = useState([]);
-    const [activeProperty, setActiveProperty] = useState(1);
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_HOST_URL}/api/properties`).then((res) => {
-            setPropertyList(res.data);
-        }).catch((err) => {
-            if (err.response.data.status !== 404) {
-                alert("no records found!");
-                return
-            }
-        })
-    }, [activeProperty]);
-    const deactive = (propertyid) => {
-        axios
-            .patch(
-                `${process.env.REACT_APP_HOST_URL}/api/properties/byId/${propertyid}`,
-                { isActive: 0 },
-                { headers: { accessToken: localStorage.getItem("accessToken") } }
-            )
-            .then((res) => {
+  const [propertyList, setPropertyList] = useState([]);
+  const [activeProperty, setActiveProperty] = useState(1);
 
-                if (res.data.error) {
-                    alert("error in approving user");
-                    return;
-                }
-                console.log(res);
-                setActiveProperty(activeProperty + 1);
-                alert("property is deactived");
-            })
-            .catch((err) => {
-                alert("error in deactive property");
-            });
-    }
-    return (
-        <div className="adminTable">
-            <Row>
-                <Col>
-                    <h1>Property Management</h1>
-                </Col>
-                <Col>
-                    <h2><Link to="/admin/users">To User Management</Link></h2>
-                </Col>
-            </Row>
-            <Table responsive bordered hover className="userTable">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>broker_id</th>
-                        <th>address</th>
-                        <th>city</th>
-                        <th>postal</th>
-                        <th>type</th>
-                        <th>isActive</th>
-                        <th>Action</th>
-                    </tr>
-                    {propertyList.map((value, key) => {
+  //Error&Message Modal section
+  const [show, setShow] = useState({ message: "", status: false });
+  const handleClose = () => {
+    setShow({ message: "", status: false });
+    window.location.reload();
+  };
+  const handleShow = (message) => setShow({ message: message, status: true });
 
-                        return (
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_HOST_URL}/api/properties`)
+      .then((res) => {
+        setPropertyList(res.data);
+      })
+      .catch((err) => {
+        if (err.response.data.status !== 404) {
+          handleShow("no records found!");
+          return;
+        }
+      });
+  }, [activeProperty]);
+  const deactive = (propertyid) => {
+    axios
+      .patch(
+        `${process.env.REACT_APP_HOST_URL}/api/properties/byId/${propertyid}`,
+        { isActive: 0 },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      )
+      .then((res) => {
+        if (res.data.error) {
+          handleShow("error in approving user");
+          return;
+        }
+        console.log(res);
+        setActiveProperty(activeProperty + 1);
+        handleShow("property is deactived");
+      })
+      .catch((err) => {
+        handleShow("error in deactive property");
+      });
+  };
+  return (
+    <div className="adminTable">
+      <Row>
+        <Col>
+          <h1>Property Management</h1>
+        </Col>
+        <Col>
+          <h2>
+            <Link to="/admin/users">To User Management</Link>
+          </h2>
+        </Col>
+      </Row>
+      <Table responsive bordered hover className="userTable">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>broker_id</th>
+            <th>address</th>
+            <th>city</th>
+            <th>postal</th>
+            <th>type</th>
+            <th>isActive</th>
+            <th>Action</th>
+          </tr>
+          {propertyList.map((value, key) => {
+            return (
+              <tr>
+                <td>{value.id}</td>
+                <td>{value.broker_id}</td>
+                <td>{value.address}</td>
+                <td>{value.city}</td>
+                <td>{value.postal}</td>
 
-                            <tr>
-                                <td>{value.id}</td>
-                                <td>{value.broker_id}</td>
-                                <td>{value.address}</td>
-                                <td>{value.city}</td>
-                                <td>{value.postal}</td>
+                <td>{value.type}</td>
 
-                                <td>{value.type}</td>
+                <td>{value.isActive}</td>
 
-                                <td>{value.isActive}</td>
-
-                                <td><Button variant="outline-warning" onClick={() => {
-                                    deactive(value.id);
-                                }}>Deactive</Button></td>
-                            </tr>
-                        )
-
-                    })}
-
-                </thead>
-            </Table>
-        </div>
-    )
+                <td>
+                  <Button
+                    variant="outline-warning"
+                    onClick={() => {
+                      deactive(value.id);
+                    }}
+                  >
+                    Deactive
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </thead>
+      </Table>
+      {/* Modal message rendering */}
+      <ModalMessage show={show} handleClose={handleClose}></ModalMessage>
+    </div>
+  );
 }
 
-export default AdminProperty
+export default AdminProperty;
